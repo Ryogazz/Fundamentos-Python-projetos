56e4a54464d7ada35747beeaaddfded8f6f8ab30
Ryogazz <josuex.x.x@hotmail.com>
2021-01-28 17:50:14 -0300

    Estruturas de controle

--- area_circulo_v1.py
+++
@@ -1,7 +0,0 @@
-Pi = 3.14159
-Raio = 15.3
-Circuferencia = 2 * Pi * Raio
-Area = Pi * Raio ** 2
-print("A circuferência do circulo e: {:.2f}".format(Circuferencia))
-print("A area do criculo e: {:.2f}".format(Area))
-
--- area_circulo_v6.py
+++
@@ -1,8 +0,0 @@
- #! /usr/bin/python3
-from math import pi
-
-if __name__ == '__main__':
-    Raio = float(input('informe o raio'))
-    Area = pi * float(Raio) ** 2
-    print("A area do criculo e: {:.2f}".format(Area))
-
