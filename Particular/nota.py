e1f46c370b93d5ea75d259df24a41992f60d62b6
Ryogazz <josuex.x.x@hotmail.com>
2021-02-04 21:24:18 -0300

    Mudnado para nota ponderada

--- Particular/nota.py
+++ Particular/nota.py
@@ -4,6 +4,6 @@ nota_1 = float(input('Digite a nota 1: '))
 nota_2 = float(input('Digite a nota 2 '))
 nota_3 = float(input('Digite a nota 3 '))
 
-media = (nota_1 + nota_2 + nota_3) / 3
+mediaP = (nota_1 * 1) + (nota_2 * 2) + (nota_3 * 3) / 6
 
-print(f'A media do aluno e {media}')
+print(f'A media poderada do aluno e {mediaP}')
