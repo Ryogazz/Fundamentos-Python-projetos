0ef17e92ada9468ce177a110a7087bb32ad7a2ee
Ryogazz <josuex.x.x@hotmail.com>
2021-02-05 15:40:49 -0300

    maior entre 2 valores

---
+++ Particular/Folha de exercicios 2/maior.py
@@ -0,0 +1,9 @@
+#! python
+
+numero1 = float(input('Digite o valor 1: '))
+numero2 = float(input('Digite o valor 2: '))
+
+if numero1 > numero2:
+    print(f'O valor 1 = {numero1} e maior')
+else:
+    print(f'O valor 2 = {numero2} e maior')
