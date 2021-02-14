96812d849709ff9d17df35b26a87005f0c2f0c4b
Ryogazz <josuex.x.x@hotmail.com>
2021-02-14 12:20:38 -0300

    forca

--- Lista de exercicios/forca.py
+++ Lista de exercicios/forca.py
@@ -1,28 +1,38 @@
 #! python
 import random
+import sys
 
 palavras = ['lua', 'casa', 'carro', 'goku', 'escola', 'celular']
 acertos = []
 palavra = random.choice(palavras)
 
-contador = 0
+chances = 5
+numeroLetra = len(palavra)
 
-for i in range(0, len(palavra)):
+for i in range(0, numeroLetra):
     acertos.append('*')
 
-while contador <= 5:
+
+while chances >= 0:
     palpite = input("\nDigite a letra do palpite: ")
     palpite = palpite.lower()
-
-    for i in range(0, len(palavra)):
-        if palpite == palavra[i]:
-            acertos[i] = palpite
-
-        print(acertos[i], end="")
-
-    for j in range(0, len(palavra)):
-        if palpite == '*':
-            contador = contador + 1
-            print('Não existe essa letra na palavra')
+    if len(palpite) > 1:
+        print('Digite somente uma letra.')
+        continue
 
 
+    for letra in range(0, numeroLetra):
+        if palpite == palavra[letra]:
+            acertos[letra] = palpite
+
+        print(acertos[letra], end="")
+
+
+    if palpite not in palavra:
+        chances -= 1
+        print('\nNão existe essa letra na palavra')
+        print(f'Você tem {chances} chances')
+
+    if chances == 0:
+        print('Forca voce perdeu')
+        sys.exit(0)
