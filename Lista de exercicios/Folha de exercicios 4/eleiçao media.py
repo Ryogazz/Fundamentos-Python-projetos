dd7bcf001aac57b2baaab4bee1504a3834da93ab
Ryogazz <josuex.x.x@hotmail.com>
2021-02-16 19:29:46 -0300

    melhorando codigo eleiçao com função

--- Lista de exercicios/Folha de exercicios 4/eleiçao media.py
+++ Lista de exercicios/Folha de exercicios 4/eleiçao media.py
@@ -3,16 +3,13 @@
 votos = []
 verificar = True
 
-try:
-    for i in range(5):
-        entrada = int(input(f'Digite a quantidade de votos da chapa {i + 1}  '))
-        votos.append(entrada)
 
-except:
-    print('Digite somente numeros inteiros')
+for i in range(5):
+    entrada = int(input(f'Digite a quantidade de votos da chapa {i + 1}  '))
+    votos.append(entrada)
 
 
-    totalDeVotos = sum(votos)
+totalDeVotos = sum(votos)
 
 def percentual(chapa):
     percentual = (chapa * 100) / totalDeVotos
@@ -39,9 +36,11 @@ def resultado(votos,percentual):
 
         return resultado
 
+def resultado_geral(percentual,votos):
+    for i in votos:
+      resultadoG = print(f'A chapa {i} recebeu {percentual(votos[i]): .2f} % dos votos, com um total de {votos[i]} votos')
+    return resultado
 
 print(resultado(votos,percentual))
-
 print('**************************************************************************')
-for i in range(5):
-    print(f'A chapa {i +1} recebeu {percentual(votos[i]): .2f} % dos votos, com um total de {votos[i]} votos')
+print(resultado_geral(percentual, votos))
