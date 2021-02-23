05a095175bede9273ec82b2aa0b42d03704455b8
Ryogazz <josuex.x.x@hotmail.com>
2021-02-22 23:33:20 -0300

    Estudo sobre recursao

---
+++ estruturas_controle_projetos/fibonacci_recursivo_v2.py
@@ -0,0 +1,12 @@
+#! python
+
+def fibonacci(quantidade, sequencia=(0, 1)):
+
+    return sequencia if len(sequencia) == quantidade \
+        else fibonacci(quantidade, sequencia + (sum(sequencia[-2:]),))
+
+
+if __name__=='__main__':
+    #listar os 20 primeiros numeros da sequencia
+    for fib in fibonacci(20):
+        print(fib)
